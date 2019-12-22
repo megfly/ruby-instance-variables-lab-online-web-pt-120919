4518e2b926eeca28925eedc8f6fb23c3d88fbd0e
Meghan Flynn <mflynn333@gmail.com>
2019-12-22 00:05:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
   
   def name=(dogs_name) #setter
-    this_dogs_name = dog_name 
+    @this_dogs_name = dog_name 
   end 
   
   def name #getter
-    this_dogs_name
+    @this_dogs_name
   end 
 end 
 
